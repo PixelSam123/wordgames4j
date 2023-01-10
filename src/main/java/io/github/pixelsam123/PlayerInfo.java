b745a62fb75fa0cf6e7931c303cbe4d6dbf27e1a
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-01-10 15:56:09 +0700

    some progress on ws/anagrams

--- src/main/java/io/github/pixelsam123/PlayerInfo.java
+++ src/main/java/io/github/pixelsam123/PlayerInfo.java
@@ -5,4 +5,9 @@ public class PlayerInfo {
     public String name;
     public int points;
 
+    public PlayerInfo(String name) {
+        this.name = name;
+        this.points = 0;
+    }
+
 }
--- src/main/java/io/github/pixelsam123/WebsocketAnagram.java
+++ src/main/java/io/github/pixelsam123/WebsocketAnagram.java
@@ -1,12 +1,13 @@
 package io.github.pixelsam123;
 
+import io.quarkus.logging.Log;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
-import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -23,18 +24,49 @@ public class WebsocketAnagram {
             Map.entry("type", "ChatMessage"),
             Map.entry("content", "Please enter name in chat.")
         ));
+
+        Log.info("New player joined, awaiting name.");
     }
 
     @OnClose
     public void onClose(Session session) {
+        broadcast(players.get(session).name + " left!");
     }
 
     @OnError
     public void onError(Session session, Throwable throwable) {
+        broadcast("ERROR: " + throwable.getMessage());
     }
 
     @OnMessage
-    public void onMessage(String message) {
+    public void onMessage(Session session, String message) {
+        PlayerInfo player = players.get(session);
+
+        if (player == null) {
+            players.put(session, new PlayerInfo(message));
+            broadcast(message + " joined!");
+
+            return;
+        }
+
+        if (message.matches("/start \\d+")) {
+            int timePerRound = Integer.parseInt(message.split(" ")[1]);
+            broadcast("Round started with time per round of " + timePerRound + " seconds!");
+
+            return;
+        }
+
+        broadcast(players.get(session).name + ": " + message);
+    }
+
+    private void broadcast(String message) {
+        for (Session playerSession : players.keySet()) {
+            playerSession.getAsyncRemote().sendObject(Map.ofEntries(
+                Map.entry("type", "ChatMessage"),
+                Map.entry("content", message)
+            ));
+        }
+        Log.info("BROADCAST -> " + message);
     }
 
 }
