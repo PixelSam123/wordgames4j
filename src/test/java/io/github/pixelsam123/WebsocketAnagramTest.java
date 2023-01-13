b60cf6547036168e3719c6d279975f2f44b4c348
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-01-13 12:29:06 +0700

    improve tests

--- src/test/java/io/github/pixelsam123/WebsocketAnagramTest.java
+++ src/test/java/io/github/pixelsam123/WebsocketAnagramTest.java
@@ -4,13 +4,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.junit.mockito.InjectMock;
+import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import javax.websocket.ClientEndpoint;
 import javax.websocket.ContainerProvider;
 import javax.websocket.OnMessage;
 import javax.websocket.Session;
 import java.net.URI;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Set;
 import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
 
@@ -25,53 +32,75 @@ public class WebsocketAnagramTest {
     @TestHTTPResource("/ws/anagram")
     public URI uri;
 
+    @InjectMock
+    @RestClient
+    public RandomWordService randomWordService;
+
+    @BeforeEach
+    public void setup() {
+        Mockito
+            .when(randomWordService.getWordsOfLength(1, 5))
+            .thenReturn(Uni.createFrom().item(Set.of("place")));
+    }
+
     @Test
     public void happyPath() throws Exception {
         try (Session session = ContainerProvider
             .getWebSocketContainer()
             .connectToServer(Client.class, uri)) {
-            ObjectNode asksForNameEntry = jsonMapper.createObjectNode();
-            asksForNameEntry.put("type", "ChatMessage");
-            asksForNameEntry.put("content", "Please enter name in chat.");
+            ObjectNode asksForNameEntry = constructJson(
+                new SimpleEntry<>("type", "ChatMessage"),
+                new SimpleEntry<>("content", "Please enter name in chat.")
+            );
 
             assertEquals(
                 asksForNameEntry,
                 jsonMapper.readTree(messages.poll(10, TimeUnit.SECONDS))
             );
 
-            ObjectNode announcesJoiningPlayer = jsonMapper.createObjectNode();
-            announcesJoiningPlayer.put("type", "ChatMessage");
-            announcesJoiningPlayer.put("content", "PlayerOne joined!");
-
+            ObjectNode announcesJoiningPlayer = constructJson(
+                new SimpleEntry<>("type", "ChatMessage"),
+                new SimpleEntry<>("content", "PlayerOne joined!")
+            );
             session.getAsyncRemote().sendText("PlayerOne");
+
             assertEquals(
                 announcesJoiningPlayer,
                 jsonMapper.readTree(messages.poll(10, TimeUnit.SECONDS))
             );
 
-            ObjectNode announcesChat = jsonMapper.createObjectNode();
-            announcesChat.put("type", "ChatMessage");
-            announcesChat.put("content", "PlayerOne: Heyy, I'm chatting!");
-
+            ObjectNode announcesChat = constructJson(
+                new SimpleEntry<>("type", "ChatMessage"),
+                new SimpleEntry<>("content", "PlayerOne: Heyy, I'm chatting!")
+            );
             session.getAsyncRemote().sendText("Heyy, I'm chatting!");
+
             assertEquals(
                 announcesChat,
                 jsonMapper.readTree(messages.poll(10, TimeUnit.SECONDS))
             );
 
-            ObjectNode announcesRoundStart = jsonMapper.createObjectNode();
-            announcesRoundStart.put("type", "ChatMessage");
-            announcesRoundStart.put(
-                "content",
-                "2 rounds started with time per round of 5 seconds!"
+            ObjectNode announcesRoundStart = constructJson(
+                new SimpleEntry<>("type", "ChatMessage"),
+                new SimpleEntry<>("content", "1 rounds started with time per round of 5 seconds!")
             );
+            session.getAsyncRemote().sendText("/start 1 5");
 
-            session.getAsyncRemote().sendText("/start 2 5");
             assertEquals(
                 announcesRoundStart,
                 jsonMapper.readTree(messages.poll(10, TimeUnit.SECONDS))
             );
         }
+    }
+
+    @SafeVarargs
+    private ObjectNode constructJson(SimpleEntry<String, String>... entries) {
+        ObjectNode json = jsonMapper.createObjectNode();
+        for (SimpleEntry<String, String> entry : entries) {
+            json.put(entry.getKey(), entry.getValue());
+        }
+
+        return json;
     }
 
     @ClientEndpoint
