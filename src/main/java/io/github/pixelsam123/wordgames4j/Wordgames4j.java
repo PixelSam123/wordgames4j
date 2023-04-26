cb7b6864859da5ad6bb7d0162ae6c4076920dc27
PixelSam123 <77851256+PixelSam123@users.noreply.github.com>
2023-04-26 16:47:54 +0700

    add Swagger UI

---
+++ src/main/java/io/github/pixelsam123/wordgames4j/Wordgames4j.java
@@ -0,0 +1,16 @@
+package io.github.pixelsam123.wordgames4j;
+
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+
+import javax.ws.rs.core.Application;
+
+@OpenAPIDefinition(
+    info = @Info(
+        title = "wordgames4j",
+        version = "1.0-SNAPSHOT",
+        description = "A word games server for WebSockets"
+    )
+)
+public class Wordgames4j extends Application {
+}
